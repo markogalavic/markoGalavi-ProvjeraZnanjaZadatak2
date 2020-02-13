ae66c7651f42033565fb7e97d64ec18d762c790f
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-02-13 10:13:14 +0100

    Add files via upload

---
+++ markoGalavićProvjeraZnanjaZadatak2/markoGalavićProvjeraZnanjaZadatak2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićProvjeraZnanjaZadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            string unos;
+            Console.WriteLine("Unesite tri različita broja");
+            unos = Console.ReadLine();
+            a = Convert.ToInt32(unos.Substring(0,2));
+            b = Convert.ToInt32(unos.Substring(3,5));
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b || a > c)
+            {
+                Console.WriteLine("Najveći broj je" + a);
+            }
+            if (b > a || b > c)
+            {
+                Console.WriteLine("Najveći broj je" + b);
+            }
+            if (c > a || c > b)
+            {
+                Console.WriteLine("Najveći broj je" + c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
